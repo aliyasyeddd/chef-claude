e125781058cc09b26c69e80888c1bbedfcefefe7
aliya mahaboob <mahaboobaliya02@gmail.com>
2025-12-01 05:02:58 +0530

    Update Main.jsx

--- src/Components/Main.jsx
+++ src/Components/Main.jsx
@@ -1,16 +1,37 @@
-import React from "react";
+import { useState } from "react";
 
 const Main = () => {
+  const [ingredients, setIngredients] = useState([
+    "Chicken",
+    "Oregano",
+    "Tomatoes",
+  ]);
+
+  const ingredientsListItems = ingredients.map((ingredient) => (
+    <li key={ingredient}>{ingredient}</li>
+  ));
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    const formData = new FormData(event.currentTarget);
+    const newIngredient = formData.get("ingredient");
+      setIngredients((prevIngredient) => [...prevIngredient, newIngredient]);
+    
+  }
+
   return (
     <main>
-      <form className="add-ingredient-form">
+      <form onSubmit={handleSubmit} className="add-ingredient-form">
         <input
           type="text"
+          
           placeholder="e.g. oregano"
           aria-label="Add ingredient"
+          name="ingredient"
         />
         <button>Add ingredient</button>
       </form>
+      <ul>{ingredientsListItems}</ul>
     </main>
   );
 };
