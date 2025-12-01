c2b7044fa273d62458c717e4f1095fba77eaf166
aliya mahaboob <mahaboobaliya02@gmail.com>
2025-12-01 21:30:43 +0530

    Add .env files to .gitignore and log API key in App
    Updated .gitignore to exclude environment files for better security. In App.jsx, added retrieval and console logging of the VITE_HUGGINGFACE_API_KEY from environment variables.

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,17 @@
-import './App.css'
-import Header from './Components/Header'
-import Main from './Components/Main'
+import "./App.css";
+import Header from "./Components/Header";
+import Main from "./Components/Main";
 
 function App() {
- 
+  const token = import.meta.env.VITE_HUGGINGFACE_API_KEY;
+  console.log("token:", token);
 
   return (
     <>
       <Header />
       <Main />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
